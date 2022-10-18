cf001d5a1a8f82daa6327c75cda0d3857b0088aa
Ujin32 <102483885+Ujin32@users.noreply.github.com>
2022-10-18 16:49:12 +0300

    Внесены изменения по результатам ревью
    1. Удалена  обёртки над requests  в виде функции get_devman_checks
    2. Высушен код программы
    3.Добавлен интервал в 30 сек между переподключениями между неудачными попытками

--- get_devman_checks.py
+++ get_devman_checks.py
@@ -1,25 +1,12 @@
 import argparse
 import os
+import time
 
 from dotenv import load_dotenv
 
 import requests
 
 import telegram
-
-
-def get_devman_checks(auth_token, timestamp_to_request=None):
-    headers = {
-        'Authorization': f'Token {auth_token}'
-    }
-    params = {
-        'timestamp': timestamp_to_request
-    }
-    devman_url = 'https://dvmn.org/api/long_polling/'
-    response = requests.get(devman_url, headers=headers, params=params)
-    response.raise_for_status()
-    devman_checks = response.json()
-    return devman_checks
 
 
 def main():
@@ -44,30 +31,27 @@ def main():
     )
     positive_message_template = 'У вас проверили работу.\n'
     'Результат: "Успешно"'
+    headers = {
+        'Authorization': f'Token {auth_token}'
+    }
+    devman_url = 'https://dvmn.org/api/long_polling/'
     while True:
         try:
-            devman_checks = get_devman_checks(auth_token)
+            response = requests.get(devman_url, headers=headers)
+            response.raise_for_status()
+            devman_checks = response.json()
             if devman_checks['status'] == 'timeout':
-                devman_checks_with_timestamp = get_devman_checks(
-                    auth_token,
-                    devman_checks['timestamp_to_request']
+                timestamp_to_request = devman_checks['timestamp_to_request']
+                params = {
+                    'timestamp': f'{timestamp_to_request}'
+                    }
+                response = requests.get(
+                    devman_url,
+                    headers=headers,
+                    params=params
                 )
-                if devman_checks_with_timestamp['status'] == 'timeout':
-                    continue
-                elif devman_checks_with_timestamp['status'] == 'found':
-                    new_attempts = devman_checks["new_attempts"]
-                    for attempt in new_attempts:
-                        if attempt["is_negative"]:
-                            lesson_title = attempt["lesson_title"]
-                            lesson_url = attempt["lesson_url"]
-                            user_message = negative_message_template.format(
-                                lesson_title,
-                                lesson_url
-                            )
-                        else:
-                            user_message = positive_message_template
-                        bot_checking_tasks.send_message(
-                            text=user_message, chat_id=args.chat_id)
+                response.raise_for_status()
+                devman_checks = response.json()
             elif devman_checks['status'] == 'found':
                 new_attempts = devman_checks["new_attempts"]
                 for attempt in new_attempts:
@@ -85,6 +69,7 @@ def main():
                         chat_id=args.chat_id
                     )
         except requests.exceptions.ReadTimeout:
+            time.sleep(30)
             continue
         except requests.exceptions.ConnectionError:
             continue
