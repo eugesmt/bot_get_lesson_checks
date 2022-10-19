85d6b76e1911fac1afa6b1755b5302d067c537ce
Ujin32 <102483885+Ujin32@users.noreply.github.com>
2022-10-19 16:57:59 +0300

    Внесены изменения
    1.Убрал лишний запрос
    2. Внесены изменения в шаблон положительного ответа о проверки работ

--- get_devman_checks.py
+++ get_devman_checks.py
@@ -29,29 +29,23 @@ def main():
         'Название урока: {}\n'
         'Ссылка на урок: {}'
     )
-    positive_message_template = 'У вас проверили работу.\n'
+    positive_message_template = 'У вас проверили работу: {}.\n'
     'Результат: "Успешно"'
     headers = {
         'Authorization': f'Token {auth_token}'
     }
     devman_url = 'https://dvmn.org/api/long_polling/'
+    timestamp_to_request = ''
+    params = {
+        'timestamp': f'{timestamp_to_request}'
+    }
     while True:
         try:
-            response = requests.get(devman_url, headers=headers)
+            response = requests.get(devman_url, headers=headers, params=params)
             response.raise_for_status()
             devman_checks = response.json()
             if devman_checks['status'] == 'timeout':
                 timestamp_to_request = devman_checks['timestamp_to_request']
-                params = {
-                    'timestamp': f'{timestamp_to_request}'
-                    }
-                response = requests.get(
-                    devman_url,
-                    headers=headers,
-                    params=params
-                )
-                response.raise_for_status()
-                devman_checks = response.json()
             elif devman_checks['status'] == 'found':
                 new_attempts = devman_checks["new_attempts"]
                 for attempt in new_attempts:
@@ -62,16 +56,19 @@ def main():
                             lesson_title,
                             lesson_url
                         )
-                    else:
-                        user_message = positive_message_template
+                    elif not attempt["is_negative"]:
+                        lesson_title = attempt["lesson_title"]
+                        user_message = positive_message_template.format(
+                            lesson_title
+                        )
                     bot_checking_tasks.send_message(
                         text=user_message,
                         chat_id=args.chat_id
                     )
         except requests.exceptions.ReadTimeout:
-            time.sleep(30)
             continue
         except requests.exceptions.ConnectionError:
+            time.sleep(30)
             continue
 
 
